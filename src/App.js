2833367ab6672df94d7d5b107803ef9dd9a8914c
Hlynur Tryggvason <h@x0r.is>
2017-11-20 21:44:51 +0000

    Adding raters name field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import BeerRaterList from './beer-rater-list/BeerRaterList';
+import RaterName from './rater-name/RaterName';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      raterName: ''
+    };
+  }
+
+  handleRaterNameChanged = newName => {
+    this.setState({ raterName: newName });
+  };
+
   render() {
     return (
       <div className="app">
+        <RaterName onNameChange={this.handleRaterNameChanged} />
         <BeerRaterList />
       </div>
     );
