5421dbe39a4c8b5595b3368d981ccad1281f3557
Hlynur Tryggvason <h@x0r.is>
2018-12-11 22:44:24 +0000

    WIP rating results

--- src/RatingResults/RatingResults.js
+++ src/RatingResults/RatingResults.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { db } from "../firebase";
 
-const RatingResults = ({ match }) => {
-  return <div>Anus {match.params.id}</div>;
-};
+class RatingResults extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.ratingSessionId = props.match.params.id;
+    this.dbUnsubscribe = null;
+
+    this.state = {
+      allRatings: []
+    };
+  }
+
+  componentDidMount() {
+    this.dbUnsubscribe = db
+      .collection("beerRatings")
+      .where("ratingSessionId", "==", this.ratingSessionId)
+      .onSnapshot(querySnapshot => {
+        const allRatings = [];
+
+        querySnapshot.foreach(document => {
+          allRatings.push(document.data());
+        });
+
+        this.setState({
+          allRatings: allRatings
+        });
+      });
+  }
+
+  componentWillUnmount() {
+    if (this.dbUnsubscribe) {
+      this.dbUnsubscribe();
+    }
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.allRatings.map(rating => (
+          <li>{rating.beerId}</li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default RatingResults;
