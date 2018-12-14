8b81a5546419693a7ec1d45d7b92db7c7a2098c6
Hlynur Tryggvason <h@x0r.is>
2018-12-14 23:33:15 +0000

    Basic dassboard

--- src/RatingResults/RatingResults.js
+++ src/RatingResults/RatingResults.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { HorizontalBar } from "react-chartjs-2";
 import { db } from "../firebase";
 import { addTotalRating, averageRatingByBeer } from "./calculator";
 
@@ -41,54 +42,105 @@ class RatingResults extends React.Component {
     let { allRatings } = this.state;
     allRatings = addTotalRating(allRatings);
 
+    const avgColors = averageRatingByBeer(allRatings, "color");
+    const avgSmell = averageRatingByBeer(allRatings, "smell");
+    const avgTaste = averageRatingByBeer(allRatings, "taste");
+    const avgXmas = averageRatingByBeer(allRatings, "xmas");
+    const avgTotal = averageRatingByBeer(allRatings, "total");
+
     return (
       <div className="rating-results">
         <h1>Hér eru niðurstöður dómnefndar</h1>
 
         <h2>Glæsilegasti bjórinn</h2>
-        <ul>
-          {averageRatingByBeer(allRatings, "color").map(b => (
-            <li>
-              {b.beerId} - {b.colorAvg}
-            </li>
-          ))}
-        </ul>
+        <HorizontalBar
+          data={{
+            labels: avgColors.map(c => c.beerId),
+            datasets: [
+              {
+                label: "Glæsilegasti bjórinn",
+                backgroundColor: "rgba(255,99,132,0.2)",
+                borderColor: "rgba(255,99,132,1)",
+                borderWidth: 1,
+                hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                hoverBorderColor: "rgba(255,99,132,1)",
+                data: avgColors.map(c => c.colorAvg)
+              }
+            ]
+          }}
+        />
 
         <h2>Fnykbesti bjórinn</h2>
-        <ul>
-          {averageRatingByBeer(allRatings, "smell").map(b => (
-            <li>
-              {b.beerId} - {b.smellAvg}
-            </li>
-          ))}
-        </ul>
+        <HorizontalBar
+          data={{
+            labels: avgSmell.map(c => c.beerId),
+            datasets: [
+              {
+                label: "Fnykbesti bjórinn",
+                backgroundColor: "rgba(23,153,14,0.2)",
+                borderColor: "rgba(23,153,14,1)",
+                borderWidth: 1,
+                hoverBackgroundColor: "rgba(23,153,14,0.4)",
+                hoverBorderColor: "rgba(23,153,14,1)",
+                data: avgSmell.map(c => c.smellAvg)
+              }
+            ]
+          }}
+        />
 
         <h2>Bragðbesti bjórinn</h2>
-        <ul>
-          {averageRatingByBeer(allRatings, "taste").map(b => (
-            <li>
-              {b.beerId} - {b.tasteAvg}
-            </li>
-          ))}
-        </ul>
+        <HorizontalBar
+          data={{
+            labels: avgTaste.map(c => c.beerId),
+            datasets: [
+              {
+                label: "Bragðbesti bjórinn",
+                backgroundColor: "rgba(0,219,255,0.2)",
+                borderColor: "rgba(0,219,255,1)",
+                borderWidth: 1,
+                hoverBackgroundColor: "rgba(0,219,255,0.4)",
+                hoverBorderColor: "rgba(0,219,255,1)",
+                data: avgTaste.map(c => c.tasteAvg)
+              }
+            ]
+          }}
+        />
 
         <h2>Mesti jólaandinn</h2>
-        <ul>
-          {averageRatingByBeer(allRatings, "xmas").map(b => (
-            <li>
-              {b.beerId} - {b.xmasAvg}
-            </li>
-          ))}
-        </ul>
+        <HorizontalBar
+          data={{
+            labels: avgXmas.map(c => c.beerId),
+            datasets: [
+              {
+                label: "Mesti jólaandinn",
+                backgroundColor: "rgba(232,150,12,0.2)",
+                borderColor: "rgba(232,150,12,1)",
+                borderWidth: 1,
+                hoverBackgroundColor: "rgba(232,150,12,0.4)",
+                hoverBorderColor: "rgba(232,150,12,1)",
+                data: avgXmas.map(c => c.xmasAvg)
+              }
+            ]
+          }}
+        />
 
         <h2>Bestur á landi hér</h2>
-        <ul>
-          {averageRatingByBeer(allRatings, "total").map(b => (
-            <li>
-              {b.beerId} - {b.totalAvg}
-            </li>
-          ))}
-        </ul>
+        <HorizontalBar
+          data={{
+            labels: avgTotal.map(c => c.beerId),
+            datasets: [
+              {
+                label: "Bestur á landi hér",
+                backgroundColor: "rgba(84,12,232,0.2)",
+                borderColor: "rgba(84,12,232,1)",
+                borderWidth: 1,
+                hoverBackgroundColor: "rgba(84,12,232,0.4)",
+                hoverBorderColor: "rgba(84,12,232,1)",
+                data: avgTotal.map(c => c.totalAvg)
+              }
+            ]
+          }}
+        />
       </div>
     );
   }
